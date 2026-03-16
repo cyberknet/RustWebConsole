89603bffcbfadfff77c72a3d7d092ef10347f5d8
CyberKnet <github@cyberknet.net>
2026-03-15 23:49:16 -0500

    Update ServerDto to match Server (without password)

--- Source/RustWebConsole.Web/Data/DTOs/ServerDto.cs
+++ Source/RustWebConsole.Web/Data/DTOs/ServerDto.cs
@@ -4,6 +4,7 @@ namespace RustWebConsole.Web.Data.DTOs
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string ConnectionDetails { get; set; } = string.Empty;
+        public string Hostname { get; set; } = string.Empty;
+        public int Port { get; set; }
     }
 }
